15820dcb18e9887d8827473d13100f263c4dbbd9
Shipy4ka <moloko2004goda@gmail.com>
2026-02-24 00:20:21 +0400

    fix: but it don't work

--- src/Cards/CardsGrid.tsx
+++ src/Cards/CardsGrid.tsx
@@ -5,5 +5,5 @@ import { Card } from "./Card";
 export const CardsGrid = () => {
     const cardIds = useStore(cardsStore, state => state.ids);
 
-    return <><p>CARDS GRID: </p>{cardIds.map(id => <div key={id}><p>CARD ID: {id}</p><Card id={+id}/></div>)}</>
+    return <div><p>CARDS GRID: </p>{cardIds.map(id => <div key={id}><p>CARD ID: {id}</p><Card id={+id}/></div>)}</div>
 }
--- src/towerState/react/index.ts
+++ src/towerState/react/index.ts
@@ -4,10 +4,24 @@ import type { StoreApi } from "../core"
 export function useStore<T>(store: T): T;
 export function useStore<T, U>(store: StoreApi<T>, selector: (state: T) => U): U;
 export function useStore<T, U>(store: StoreApi<T>, selector?: (state: T) => U) {
-    const slice = useSyncExternalStore(store.subscribe, useCallback(() => {
+    const getSnapshot = useCallback(() => {
         const state = store.getState();
         return selector ? selector(state) : state;
-    }, [store, selector]),);
+    }, [store, selector]);
+
+    const subscribe = (onStoreChanged: () => void) => {
+        return store.subscribe((state, prevState) => {
+            const prevSlice = selector ? selector(prevState) : prevState;
+            const newSlice = selector ? selector(state) : state;
+
+            if (!Object.is(prevSlice, newSlice)) {
+                console.log('[SUBSCRIBE] notify React')
+                onStoreChanged();
+            }
+        })
+    }
+
+    const slice = useSyncExternalStore(subscribe, getSnapshot);
     
     useDebugValue(slice);
     
